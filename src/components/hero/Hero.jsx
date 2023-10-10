bdab6331961443a915c32f243d9d0c16eec4f915
RezaMSBD <msrbdinfo@gmail.com>
2023-10-11 00:10:01 +0600

    nothing changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,15 +6,19 @@ const Hero = () => {
   return (
     <div className="hero-main">
       <div className="hero-left">
-        <h1>
-          THIS IS THE <br />
-          HERO SECTION
-        </h1>
+        <h1>THIS IS THE HERO SECTION</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.{" "}
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          provident explicabo distinctio! A fugiat natus sint odit quidem in
+          omnis ea vero sit, dolorem quisquam ab quas porro, cum veritatis eum
+          accusamus! Veritatis dolorum dolore ipsam enim tempora dolor illo quae
+          nihil a sequi obcaecati cupiditate, odio cumque. Alias dolores,
+          corporis perferendis sapiente qui, ad impedit fuga, cum ab similique
+          vitae maxime quod suscipit temporibus maiores debitis! Alias facilis
+          iusto dicta tenetur, tempora, sit ipsum sed temporibus natus beatae ad
+          exercitationem fuga commodi qui nam quidem expedita eaque perspiciatis
+          vero? Et molestias eaque impedit facilis corporis expedita dolorum
+          aliquam odio.
         </p>
 
         <div className="serchButton">
@@ -22,8 +26,8 @@ const Hero = () => {
             className="input-button"
             type="text"
             placeholder="What is your mind"
-          ></input>
-          <button>Search</button>
+          />
+          <button className="btn">Search</button>
         </div>
       </div>
 
